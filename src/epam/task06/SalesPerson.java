12b4559b2aed8a30fcf5c6f9b31f2c9491bfe047
Taras Kozodoi <acateely@gmail.com>
2021-11-23 19:00:40 +0200

    Added task06 with unit tests

---
+++ src/epam/task06/SalesPerson.java
@@ -0,0 +1,22 @@
+package epam.task06;
+
+import java.math.BigDecimal;
+
+public class SalesPerson extends Employee {
+    private int percent;
+
+    public SalesPerson(String name, BigDecimal salary, int percent) {
+        super(name, salary);
+        this.percent = percent;
+    }
+
+    @Override
+    public void setBonus() {
+        if(percent > 200){
+            setBonus(getBonus().multiply(new BigDecimal(3)));
+        }
+        else if(percent > 100){
+            setBonus(getBonus().multiply(new BigDecimal(2)));
+        }
+    }
+}
---
+++ src/epam/task06/test/epam/task06/SalesPersonTest.java
@@ -0,0 +1,38 @@
+package epam.task06;
+
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SalesPersonTest {
+
+    @Test
+    void setBonusless100() {
+        SalesPerson person = new SalesPerson("Taras", new BigDecimal(15000), 50);
+        person.setBonus(new BigDecimal(1000));
+        person.setBonus();
+        assertEquals(new BigDecimal(1000), person.getBonus());
+    }
+
+    @Test
+    void setBonusbiger100() {
+        SalesPerson person = new SalesPerson("Taras", new BigDecimal(15000), 120);
+        person.setBonus(new BigDecimal(1000));
+        person.setBonus();
+        assertEquals(new BigDecimal(2000), person.getBonus());
+    }
+
+    @Test
+    void setBonusbiger200() {
+        SalesPerson person = new SalesPerson("Taras", new BigDecimal(15000), 210);
+        person.setBonus(new BigDecimal(1000));
+        person.setBonus();
+        assertEquals(new BigDecimal(3000), person.getBonus());
+    }
+
+    @Test
+    void setBonus() {
+    }
+}
