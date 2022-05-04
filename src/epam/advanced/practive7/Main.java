50f69e4a360d97881f51bddbdc36370bbc80cfa2
Taras Kozodoi <acateely@gmail.com>
2022-05-04 21:27:41 +0300

    added third funcion

--- src/epam/advanced/practive7/Main.java
+++ src/epam/advanced/practive7/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) throws SQLException {
         SqlService service = new SqlService(CreateConnection.createStatement());
         //service.showFilmYear(2022);
-        service.showActorsInFilm("Iron Man");
+        //service.showActorsInFilm("Iron Man");
+        service.showActorsWithCount(2);
     }
 }
--- src/epam/advanced/practive7/SqlService.java
+++ src/epam/advanced/practive7/SqlService.java
@@ -1,5 +1,7 @@
 package epam.advanced.practive7;
 
+import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonIntegerFormatVisitor;
+
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -12,8 +14,8 @@ public class SqlService {
     }
 
     public void showFilmYear(int year) throws SQLException {
-        ResultSet resultSet = statement.executeQuery("select distinct title from film where release_year>=" + (year-1));
-        while (resultSet.next()){
+        ResultSet resultSet = statement.executeQuery("select distinct title from film where release_year>=" + (year - 1));
+        while (resultSet.next()) {
             System.out.println(resultSet.getString("title"));
         }
     }
@@ -21,18 +23,59 @@ public class SqlService {
     public void showActorsInFilm(String film) throws SQLException {
         ResultSet resultSet = statement.executeQuery(
                 "select actor.actor_id, actor.first_name, actor.last_name, actor.birdsyear\n" +
-                "from actor\n" +
-                "left join film_actor\n" +
-                "on film_actor.actor_id=actor.actor_id\n" +
-                "left join film\n" +
-                "on film.film_id=film_actor.film_id\n" +
-                "where title='"+ film +"'");
+                        "from actor\n" +
+                        "left join film_actor\n" +
+                        "on film_actor.actor_id=actor.actor_id\n" +
+                        "left join film\n" +
+                        "on film.film_id=film_actor.film_id\n" +
+                        "where title='" + film + "'");
 
-        while (resultSet.next()){
+        while (resultSet.next()) {
             System.out.print(resultSet.getInt("actor.actor_id") + " ");
             System.out.print(resultSet.getString("actor.first_name") + " ");
             System.out.print(resultSet.getString("actor.last_name") + " ");
             System.out.print(resultSet.getInt("actor.birdsyear") + "\n");
+        }
+    }
+
+    public void showActorsWithCount(int count) throws SQLException {
+        ResultSet resultSet = statement.executeQuery(
+                "select actor.*\n" +
+                        "from actor\n" +
+                        "left join film_actor on film_actor.actor_id=actor.actor_id\n" +
+                        "where film_id > 0"
+        );
+
+        int replicaCount = 1;
+
+        int oldId = 0;
+        String oldFirstName = "";
+        String oldLastName = "";
+        int oldYear = 0;
+
+
+        int id;
+        String firstName;
+        String lastName;
+        int year;
+
+        while (resultSet.next()) {
+            id = resultSet.getInt("actor.actor_id");
+            firstName = resultSet.getString("actor.first_name");
+            lastName = resultSet.getString("actor.last_name");
+            year = resultSet.getInt("actor.birdsyear");
+            if (id == oldId) {
+                replicaCount++;
+            } else {
+                if (replicaCount >= count) {
+                    System.out.println(oldId + " " + oldFirstName + " " + oldLastName + " " + oldYear);
+                }
+                replicaCount = 1;
+            }
+            oldId = id;
+            oldFirstName = firstName;
+            oldLastName = lastName;
+            oldYear = year;
         }
     }
 }
