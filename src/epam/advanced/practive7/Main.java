dd9d087ae4975588acece05e03d5f01050c5f265
Taras Kozodoi <acateely@gmail.com>
2022-05-04 21:13:21 +0300

    added second funciton

--- src/epam/advanced/practive7/Main.java
+++ src/epam/advanced/practive7/Main.java
@@ -5,6 +5,7 @@ import java.sql.*;
 public class Main {
     public static void main(String[] args) throws SQLException {
         SqlService service = new SqlService(CreateConnection.createStatement());
-        service.showFilmYear(2022);
+        //service.showFilmYear(2022);
+        service.showActorsInFilm("Iron Man");
     }
 }
--- src/epam/advanced/practive7/SqlService.java
+++ src/epam/advanced/practive7/SqlService.java
@@ -17,4 +17,22 @@ public class SqlService {
             System.out.println(resultSet.getString("title"));
         }
     }
+
+    public void showActorsInFilm(String film) throws SQLException {
+        ResultSet resultSet = statement.executeQuery(
+                "select actor.actor_id, actor.first_name, actor.last_name, actor.birdsyear\n" +
+                "from actor\n" +
+                "left join film_actor\n" +
+                "on film_actor.actor_id=actor.actor_id\n" +
+                "left join film\n" +
+                "on film.film_id=film_actor.film_id\n" +
+                "where title='"+ film +"'");
+
+        while (resultSet.next()){
+            System.out.print(resultSet.getInt("actor.actor_id") + " ");
+            System.out.print(resultSet.getString("actor.first_name") + " ");
+            System.out.print(resultSet.getString("actor.last_name") + " ");
+            System.out.print(resultSet.getInt("actor.birdsyear") + "\n");
+        }
+    }
 }
